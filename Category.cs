4f4b258450a07569ccaf01744dbd6f5cb07f16df
Mahmoud medhat <mahmoudmodric9@gmail.com>
2025-05-28 11:12:56 -0500

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace NotApplication2
+{
+    internal class Category
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+      static  public List<Category> Categorys = new List<Category>()
+        {
+
+
+new Category {id=1, Name = "Personal" },
+new Category {id=2, Name = "Work" },
+new Category {id=3, Name = "Study" },
+new Category {id=4, Name = "Ideas" }
+        };
+
+
+        public override string ToString()
+        {
+            return $"{id} , {Name}";
+        }
+    }
+}
