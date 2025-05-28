8d34f2013b9e8e1f3256dc599455b1eb4f76b1d7
abanoub <ezzatabanoup@gmail.com>
2025-05-28 03:19:18 +0300

    first commit

---
+++ Screens/NewUser.cs
@@ -0,0 +1,36 @@
+﻿using NotApplication2.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotApplication2.Screens
+{
+    public partial class NewUser : Form
+    {
+        WindowAppEntities we = new WindowAppEntities();
+
+
+        public NewUser()
+        {
+            InitializeComponent();
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            User user = new User();
+            user.UserName = UserNameText.Text;
+            user.Password = PassText.Text;
+
+            we.Users.Add(user);
+
+
+            we.SaveChanges();
+        }
+    }
+}
